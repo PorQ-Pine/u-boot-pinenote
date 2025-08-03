6c6efd18c2b8801214ef34bdc97dfa64b555f1f8
Szybet <53944559+Szybet@users.noreply.github.com>
2025-08-03 18:09:45 +0200

    Delete compile.sh

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-if [ -z "${CROSS_COMPILE}" ]; then echo "CROSS_COMPILE environment variable is not defined!"; exit 1; fi
-make rk3566-pinenote_defconfig
-./make.sh "${2}"
-./make.sh trust
